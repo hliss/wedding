b2c6cb6d0a5d491942bd9164ee3ad0b66894aad0
Harold Liss <harold.liss@gmail.com>
2013-07-02 22:10:10 -0700

    Trailing whitespace ectomy. Had added a few extra spaces into the rsvp textarea

--- assets/wedding.js
+++ assets/wedding.js
@@ -150,8 +150,8 @@ jQuery(function ($) {
     };
 
     // Load the SDK's source Asynchronously
-    // Note that the debug version is being actively developed and might 
-    // contain some type checks that are overly strict. 
+    // Note that the debug version is being actively developed and might
+    // contain some type checks that are overly strict.
     // Please report such bugs using the bugs tool.
     (function(d, debug){
         var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
